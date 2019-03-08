7f7c058128afce8bbce58a7fca074fb312c852f1
Michael Latiolais <mike@latiolais.net>
2019-03-08 07:36:21 +0000

    Edited to compile with OPPA_X and OPPA vid

--- src/unix/video.c
+++ src/unix/video.c
@@ -215,7 +215,7 @@ struct rc_option video_opts[] = {
 };
 
 
-#ifndef LISY_VIDEO
+#if !defined(LISY_VIDEO) && !defined(OPPA_VIDEO)
 static int video_handle_scale(struct rc_option *option, const char *arg,
    int priority)
 {
@@ -1210,6 +1210,99 @@ void SetThrottleAdj(int adj)
 
 //  LISY dummy/fake video driver
 #ifdef LISY_VIDEO
+
+void osd_pause(int paused)
+{
+        emulation_paused = paused;
+}
+
+void osd_close_display(void)
+{
+}
+
+static int skip_next_frame = 0;
+int osd_skip_this_frame(void)
+{
+        return skip_next_frame;
+}
+
+#ifndef xgl
+struct mame_bitmap *osd_override_snapshot(struct mame_bitmap *bitmap,
+                struct rectangle *bounds)
+{
+        struct mame_bitmap *copy;
+        return copy;
+}
+#endif
+
+const char *osd_get_fps_text(const struct performance_info *performance)
+{
+        static char buffer[1024];
+ /* return a pointer to the static buffer */
+        return buffer;
+}
+
+int osd_create_display(const struct osd_create_params *params,
+                UINT32 *rgb_components)
+{
+  return 0;
+}
+
+/* Update the display. */
+void osd_update_video_and_audio(struct mame_display *display)
+{
+
+	//RTH: we do sound update in lisy80
+	//if (sound_stream && sound_enabled)
+	//sound_stream_update(sound_stream);
+}
+
+void osd_video_initpre()
+{
+}
+
+static int video_verify_bpp(struct rc_option *option, const char *arg,
+   int priority)
+{
+  return 0;
+}
+
+static int video_handle_scale(struct rc_option *option, const char *arg,
+   int priority)
+{
+  return 0;
+}
+
+static int video_verify_beam(struct rc_option *option, const char *arg,
+                int priority)
+{
+  return 0;
+}
+
+static int video_verify_flicker(struct rc_option *option, const char *arg,
+                int priority)
+{
+  return 0;
+}
+
+static int video_verify_vectorres(struct rc_option *option, const char *arg,
+   int priority)
+{
+  return 0;
+}
+
+static int video_verify_intensity(struct rc_option *option, const char *arg,
+                int priority)
+{
+        options.vector_intensity = f_intensity;
+        option->priority = priority;
+        return 0;
+}
+
+#endif
+
+//  OPPA dummy/fake video driver
+#ifdef OPPA_VIDEO
 
 void osd_pause(int paused)
 {
