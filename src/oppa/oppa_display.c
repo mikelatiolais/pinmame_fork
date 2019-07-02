730994fc083e8eabc3e9c978d46a96f6b40c3d64
Michael Latiolais <mike@latiolais.net>
2019-07-02 07:44:54 +0000

    Adding threading

--- src/oppa/oppa_display.c
+++ src/oppa/oppa_display.c
@@ -28,35 +28,54 @@ void oppaInitDMD() {
   digitalWrite(pinColLatch, LOW);
   digitalWrite(pinDotClock, LOW);
   digitalWrite(pinDotData, LOW);
+
+  /* Start DMD thread */
+  
 }
 
 void oppaBlankDMD() {
   digitalWrite(pinDisplayEnable, LOW);
 }
 
-/* Set up initial DMD pin configuration */
-void oppaInitDMDSPI() {
-  int fd;
+/* Take in array and update the DMD directly */
+void oppaUpdateDMDold(UINT8 *dotData) {
+  //printf("In update DMD\n");
+ 
+  int row; 
+  for(row = 0; row < OPPA_NUM_OF_ROWS; row++) {
+    int index;
+    /* From Vishay doc: Once each frame the ROW DATA must be asserted to synchronize the column serial data with the beginning row */
+    /* Latch the row of data */
+    digitalWrite(pinColLatch, LOW);
+    /* 128 dots / 8 (size of byte) = 16 */ 
+    for(index = 0; index < 16; index++) {
+      shiftOutSlow(pinDotData,pinDotClock,LSBFIRST,dotData[(row*16) + index]); 
+      //shiftOut(pinDotData,pinDotClock,LSBFIRST,3); 
+      //delayMicroseconds(40);
+    }
+    if(row == 0) {
+      digitalWrite(pinRowData, HIGH);
+    } else {
+      digitalWrite(pinRowData, LOW);
+    }
 
-  printf("In init SPI\n");
-  wiringPiSetup();
-  pinMode(pinDisplayEnable, OUTPUT);
-  pinMode(pinRowData, OUTPUT);
-  pinMode(pinRowClock, OUTPUT);
-  pinMode(pinColLatch, OUTPUT);
-  //pinMode(pinDotClock, OUTPUT);
-  //pinMode(pinDotData, OUTPUT);
 
-  /* Drive everything low to start */
-  digitalWrite(pinDisplayEnable, LOW);
-  digitalWrite(pinRowData, LOW);
-  digitalWrite(pinRowClock, HIGH);
-  digitalWrite(pinColLatch, LOW);
-  //digitalWrite(pinDotClock, LOW);
-  //digitalWrite(pinDotData, LOW);
+    /* Turn off the display while we latch in the this row */
+    digitalWrite(pinDisplayEnable, LOW);  
+    digitalWrite(pinColLatch, HIGH);
 
-  /* Set up SPI */
-  fd = wiringPiSPISetup(1, 500000);
+
+    /* Advance the row pointer */
+    digitalWrite(pinRowClock, LOW);   
+    /* Minimum 1us dip */
+    delayMicroseconds(0);  
+    digitalWrite(pinRowClock, HIGH);
+
+    /* Reenable display */
+    digitalWrite(pinDisplayEnable, HIGH); 
+    delayMicroseconds(2);  
+
+  }
 }
 
 /* Take in array and update the DMD directly */
@@ -64,6 +83,11 @@ void oppaUpdateDMD(UINT8 *dotData) {
   //printf("In update DMD\n");
  
   int row; 
+  /* Row data - This signal is the first line marker for the scan.
+     This input should be held high to correspond to the first row
+     of pixel data 
+  */
+  digitalWrite(pinRowData, HIGH);
   for(row = 0; row < OPPA_NUM_OF_ROWS; row++) {
     int index;
     /* From Vishay doc: Once each frame the ROW DATA must be asserted to synchronize the column serial data with the beginning row */
@@ -96,48 +120,6 @@ void oppaUpdateDMD(UINT8 *dotData) {
     digitalWrite(pinDisplayEnable, HIGH); 
     delayMicroseconds(2);  
 
-  }
-}
-
-/* Take in array and update the DMD directly */
-void oppaUpdateDMDSPI(UINT8 *dotData) {
-  int result;
-
-  //printf("In update SPI\n");
-  int row; 
-  for(row = 0; row < OPPA_NUM_OF_ROWS; row++) {
-    int index;
-    int result;
-    /* From Vishay doc: Once each frame the ROW DATA must be asserted to synchronize the column serial data with the beginning row */
-    /* Latch the row of data */
-    digitalWrite(pinColLatch, LOW);
-    result = wiringPiSPIDataRW(1, dotData, 16);
-    //for(index = 0; index < 16; index++) {
-      //shiftOut(pinDotData,pinDotClock,LSBFIRST,dotData[(row*16) + index]); 
-      //shiftOut(pinDotData,pinDotClock,LSBFIRST,3); 
-      //delayMicroseconds(40);
-    //}
-    if(row == 0) {
-      digitalWrite(pinRowData, HIGH);
-    } else {
-      digitalWrite(pinRowData, LOW);
-    }
-
-
-    /* Turn off the display while we latch in the this row */
-    digitalWrite(pinDisplayEnable, LOW);  
-    digitalWrite(pinColLatch, HIGH);
-
-
-    /* Advance the row pointer */
-    digitalWrite(pinRowClock, LOW);   
-    /* Minimum 1us dip */
-    //delayMicroseconds(10);  
-    digitalWrite(pinRowClock, HIGH);
-
-    /* Reenable display */
-    digitalWrite(pinDisplayEnable, HIGH); 
-    //delayMicroseconds(1);
   }
 }
 
